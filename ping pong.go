70d50f98ca536b6bea26a8c110bf45d43aaa0dab
Singamalaprasanthi <73575651+Singamalaprasanthi@users.noreply.github.com>
2020-11-13 14:33:39 +0530

    Add files via upload

---
+++ ping pong.go
@@ -0,0 +1,57 @@
+
+// 1. ping pong
+
+package main
+ 
+import (
+	"fmt"
+	"time"
+)
+ 
+func main() {
+	ping := make(chan struct{}, 1)
+	pong := make(chan struct{}, 1)
+ 
+	ping<- struct{}{}
+ 
+	go play(ping, pong)
+ 
+	time.Sleep(time.Millisecond)
+}
+ 
+func play(ping, pong chan struct{}) {
+	for {
+		select {
+		case <-ping:
+			fmt.Println("ping")
+			pong<- struct{}{}
+		case <-pong:
+			fmt.Println("    pong")
+			ping<- struct{}{}
+		}
+	}
+}
+
+$go run main.go
+
+ping
+    pong
+ping
+    pong
+ping
+    pong
+ping
+    pong
+ping
+    pong
+ping
+    pong
+ping
+    pong
+ping
+    pong
+ping
+    pong
+ping
+    pong
+
